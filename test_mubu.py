1d3eaf43b64d212064bea23ded660e2fd43174ec
jj1165922611 <1165922611@qq.com>
2021-01-18 21:28:02 +0800

    init

---
+++ test_mubu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/18 21:23
+# @Author  : Joey Jiang
+# @Site    : 
+# @File    : test_mubu.py
+# @Software: PyCharm
+import pytest
+def test_mubu():
+
+    print("hello world")
